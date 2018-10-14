1db26787c978eef99592d50e3617a60633dd8b12
Max Jocinei <max@sou.faccat.br>
2018-10-14 04:51:50 +0000

    Formatando data

--- Discursiva_2/calcular.php
+++ Discursiva_2/calcular.php
@@ -8,46 +8,49 @@
     <body>
         <div class="container">
             <div class="resp">
-                <?php
-                    require ("pessoa.php");
-                    
-                    $pessoa = new Pessoa();
-                    
-                    $pessoa->setNome($_POST['nome']);
-                    $pessoa->setSexo($_POST['sexo']);
-                    $pessoa->setDataNascimento($_POST['dataNascimento']);
-                    $pessoa->setPeso($_POST['peso']);
-                    $pessoa->setAltura($_POST['altura']);
-                    
-                    echo "Seja bem vindo ". $pessoa->getNome()."</br>";
-                    echo "Indivíduo do sexo ". $pessoa->getSexo()."</br>";
-                    echo "Nascido em ". $pessoa->getDataNascimento()."</br>";
-                    
-                    $imc = $pessoa->getPeso()/($pessoa->getAltura()*$pessoa->getAltura());
-                    
-                    
-                    
-                    if($imc<16){
-                        $categoria = "Magreza Grave";
-                    }elseif($imc<17){
-                        $categoria = "Magreza Moderada";
-                    }elseif($imc<18.5){
-                        $categoria = "Magreza Leve";
-                    }elseif($imc<25){
-                        $categoria = "Saudável";
-                    }elseif($imc<30){
-                        $categoria = "Sobrepeso";
-                    }elseif($imc<35){
-                        $categoria = "Obesidade Grau I";
-                    }elseif($imc<40){
-                        $categoria = "Obesidade Grau II";
-                    }elseif($imc>=40){
-                        $categoria = "Obesidade Grau III";
-                    }
-                    
-                    echo "Seu IMC é ".$imc ." e você está com ".$categoria."</br>";
-                    echo "Resultado obtido a partir do peso ".$pessoa->getPeso()."Kg e da altura ".$pessoa->getAltura()."m</br>";
-                ?>
+                <div class="formato">
+                    <?php
+                        require ("pessoa.php");
+                        
+                        $pessoa = new Pessoa();
+                        
+                        $pessoa->setNome($_POST['nome']);
+                        $pessoa->setSexo($_POST['sexo']);
+                        $pessoa->setDataNascimento($_POST['dataNascimento']);
+                        $pessoa->setPeso($_POST['peso']);
+                        $pessoa->setAltura($_POST['altura']);
+                        
+                        echo "Seja bem vindo ". $pessoa->getNome()."</br>";
+                        echo "Indivíduo do sexo ". $pessoa->getSexo()."</br>";
+                        echo "Nascido em ". date('d/m/Y ', strtotime($pessoa->getDataNascimento()))."</br>";
+                        
+                        $imc = $pessoa->getPeso()/($pessoa->getAltura()*$pessoa->getAltura());
+                        
+                        
+                        
+                        if($imc<16){
+                            $categoria = "Magreza Grave";
+                        }elseif($imc<17){
+                            $categoria = "Magreza Moderada";
+                        }elseif($imc<18.5){
+                            $categoria = "Magreza Leve";
+                        }elseif($imc<25){
+                            $categoria = "Saudável";
+                        }elseif($imc<30){
+                            $categoria = "Sobrepeso";
+                        }elseif($imc<35){
+                            $categoria = "Obesidade Grau I";
+                        }elseif($imc<40){
+                            $categoria = "Obesidade Grau II";
+                        }elseif($imc>=40){
+                            $categoria = "Obesidade Grau III";
+                        }
+                        
+                        echo "Seu IMC é ".$imc ." e você está com ".$categoria."</br>";
+                        echo "Resultado obtido a partir do peso ".$pessoa->getPeso()."Kg e da altura ".$pessoa->getAltura()."m</br>";
+                        echo "<a href='index.php'><input type='button' value='Voltar'></a>"
+                    ?>
+                </div>
             </div>
         </div>
     </body>
